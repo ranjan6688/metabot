35114a546a2f4488d6ea1855e100972f8d81a7b7
biswaranjan.nayak <Krishna@6688>
2024-07-29 18:48:43 +0530

    tmpl6

--- dist/index.js
+++ dist/index.js
@@ -9,14 +9,15 @@ const request_1 = __importDefault(require("request"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+const { META_URL, META_ACCESS_TOKEN, META_VERIFY_TOKEN } = process.env;
 function sendMessage(to, msg) {
     return new Promise((err, res) => {
         let options = {
-            url: process.env.META_URL,
+            url: META_URL,
             method: `POST`,
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.META_ACCESS_TOKEN}`
+                'Authorization': `Bearer ${META_ACCESS_TOKEN}`
             },
             body: JSON.stringify({
                 "messaging_product": "whatsapp",
@@ -51,6 +52,121 @@ function sendMessage(to, msg) {
         });
     });
 }
+function sendReplyMessage(from, msg) {
+    return new Promise((err, res) => {
+        let options = {
+            url: `https://graph.facebook.com/v18.0/${from}/messages`,
+            method: `POST`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${META_ACCESS_TOKEN}`
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                "to": `${msg.from}`,
+                text: { body: "Echo: " + msg.text.body },
+                context: {
+                    message_id: msg.id, // shows the message as a reply to the original user message
+                }
+            })
+        };
+        (0, request_1.default)(options, (error, response, body) => {
+            if (error) {
+                return err(error);
+            }
+            if (response) {
+                return res(response);
+            }
+        });
+    });
+}
+function sendMarkAsRead(from, msg) {
+    return new Promise((err, res) => {
+        let options = {
+            url: `https://graph.facebook.com/v18.0/${from}/messages`,
+            method: `POST`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${META_ACCESS_TOKEN}`
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                status: "read",
+                message_id: msg.id,
+            })
+        };
+        (0, request_1.default)(options, (error, response, body) => {
+            if (error) {
+                return err(error);
+            }
+            if (response) {
+                return res(response);
+            }
+        });
+    });
+}
+app.post("/webhook", async (req, res) => {
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j;
+    // log incoming messages
+    console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
+    // check if the webhook request contains a message
+    // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+    const message = (_e = (_d = (_c = (_b = (_a = req.body.entry) === null || _a === void 0 ? void 0 : _a[0]) === null || _b === void 0 ? void 0 : _b.changes[0]) === null || _c === void 0 ? void 0 : _c.value) === null || _d === void 0 ? void 0 : _d.messages) === null || _e === void 0 ? void 0 : _e[0];
+    // check if the incoming message contains text
+    if ((message === null || message === void 0 ? void 0 : message.type) === "text") {
+        // extract the business number to send the reply from it
+        const business_phone_number_id = (_j = (_h = (_g = (_f = req.body.entry) === null || _f === void 0 ? void 0 : _f[0].changes) === null || _g === void 0 ? void 0 : _g[0].value) === null || _h === void 0 ? void 0 : _h.metadata) === null || _j === void 0 ? void 0 : _j.phone_number_id;
+        // send a reply message as per the docs here https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
+        // await axios({
+        //   method: "POST",
+        //   url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+        //   headers: {
+        //     Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+        //   },
+        //   data: {
+        //     messaging_product: "whatsapp",
+        //     to: message.from,
+        //     text: { body: "Echo: " + message.text.body },
+        //     context: {
+        //       message_id: message.id, // shows the message as a reply to the original user message
+        //     },
+        //   },
+        // });
+        sendReplyMessage(business_phone_number_id, message).then(response => res.status(200).send(response)).catch(error => res.status(500).send(error));
+        // mark incoming message as read
+        // await axios({
+        //   method: "POST",
+        //   url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+        //   headers: {
+        //     Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+        //   },
+        //   data: {
+        //     messaging_product: "whatsapp",
+        //     status: "read",
+        //     message_id: message.id,
+        //   },
+        // });
+        sendMarkAsRead(business_phone_number_id, message).then(response => res.status(200).send(response)).catch(error => res.status(500).send(error));
+    }
+    //   res.sendStatus(200);
+});
+// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get("/webhook", (req, res) => {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+    // check the mode and token sent are correct
+    if (mode === "subscribe" && token === META_VERIFY_TOKEN) {
+        // respond with 200 OK and challenge token from the request
+        res.status(200).send(challenge);
+        console.log("Webhook verified successfully!");
+    }
+    else {
+        // respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+    }
+});
 app.get('/', (req, res) => {
     return res.status(200).send("Welcome to METABOT");
 });
@@ -70,7 +186,7 @@ app.get('/webhook', (req, res) => {
     let token = req.query["hub.verify_token"];
     console.log(mode, challange, token);
     if (mode && token) {
-        if (mode === "subscribe" && token === process.env.META_VERIFY_TOKEN) {
+        if (mode === "subscribe" && token === META_VERIFY_TOKEN) {
             console.log('success');
             return res.status(200).send(challange);
         }
--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,19 @@ import stringify = require("json-stringify-safe");
 
 env.config();
 
-const app = express()
-const port = process.env.PORT || 8080
+const app = express();
+const port = process.env.PORT || 8080;
+const { META_URL, META_ACCESS_TOKEN, META_VERIFY_TOKEN } = process.env;
 
 function sendMessage(to: string, msg: string){
 
     return new Promise((err:any, res: any) => {
         let options = {
-            url: process.env.META_URL,
+            url: META_URL,
             method: `POST`,
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.META_ACCESS_TOKEN}`
+                'Authorization': `Bearer ${META_ACCESS_TOKEN}`
             },
             body: JSON.stringify({
                 "messaging_product": "whatsapp",
@@ -55,6 +56,143 @@ function sendMessage(to: string, msg: string){
     });
 }
 
+
+function sendReplyMessage(from: string, msg: any){
+
+    return new Promise((err:any, res: any) => {
+        let options = {
+            url: `https://graph.facebook.com/v18.0/${from}/messages`,
+            method: `POST`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${META_ACCESS_TOKEN}`
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                "to": `${msg.from}`,
+                text: { body: "Echo: " + msg.text.body },
+                context: {
+                  message_id: msg.id, // shows the message as a reply to the original user message
+                }
+              })
+        };
+        
+        request(options, (error: any, response: any, body: any) => {        
+            if (error) {
+                return err(error);
+            }
+        
+            if (response) {
+                return res(response);
+            }
+        });
+
+    });
+}
+
+
+function sendMarkAsRead(from: string, msg: any){
+
+    return new Promise((err:any, res: any) => {
+        let options = {
+            url: `https://graph.facebook.com/v18.0/${from}/messages`,
+            method: `POST`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${META_ACCESS_TOKEN}`
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                status: "read",
+                message_id: msg.id,
+              })
+        };
+        
+        request(options, (error: any, response: any, body: any) => {        
+            if (error) {
+                return err(error);
+            }
+        
+            if (response) {
+                return res(response);
+            }
+        });
+
+    });
+}
+
+
+app.post("/webhook", async (req, res) => {
+  // log incoming messages
+  console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
+
+  // check if the webhook request contains a message
+  // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+
+  // check if the incoming message contains text
+  if (message?.type === "text") {
+    // extract the business number to send the reply from it
+    const business_phone_number_id =
+      req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+
+    // send a reply message as per the docs here https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
+    // await axios({
+    //   method: "POST",
+    //   url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+    //   headers: {
+    //     Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+    //   },
+    //   data: {
+    //     messaging_product: "whatsapp",
+    //     to: message.from,
+    //     text: { body: "Echo: " + message.text.body },
+    //     context: {
+    //       message_id: message.id, // shows the message as a reply to the original user message
+    //     },
+    //   },
+    // });
+
+    sendReplyMessage(business_phone_number_id, message).then(response => res.status(200).send(response)).catch(error => res.status(500).send(error));
+
+    // mark incoming message as read
+    // await axios({
+    //   method: "POST",
+    //   url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+    //   headers: {
+    //     Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+    //   },
+    //   data: {
+    //     messaging_product: "whatsapp",
+    //     status: "read",
+    //     message_id: message.id,
+    //   },
+    // });
+    
+    sendMarkAsRead(business_phone_number_id, message).then(response => res.status(200).send(response)).catch(error => res.status(500).send(error));
+  }
+
+//   res.sendStatus(200);
+});
+
+// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // check the mode and token sent are correct
+  if (mode === "subscribe" && token === META_VERIFY_TOKEN) {
+    // respond with 200 OK and challenge token from the request
+    res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
+  } else {
+    // respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
+  }
+});
+
 app.get('/', (req: Request, res: Response) => {
     return res.status(200).send("Welcome to METABOT");
 });
@@ -82,7 +220,7 @@ app.get('/webhook', (req: Request, res: Response) => {
 
     if (mode && token) {
 
-        if (mode === "subscribe" && token === process.env.META_VERIFY_TOKEN) {
+        if (mode === "subscribe" && token === META_VERIFY_TOKEN) {
             console.log('success');
             return res.status(200).send(challange);
         } else {

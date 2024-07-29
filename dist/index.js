e76ed0331f4f2eb1d636c95fa0dc872eabc0e619
biswaranjan.nayak <Krishna@6688>
2024-07-29 15:58:10 +0530

    v290724

--- dist/index.js
+++ dist/index.js
@@ -4,29 +4,85 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const request_1 = __importDefault(require("request"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
-app.get('/', (request, response) => {
-    return response.status(200).send("Welcome to METABOT");
+function sendMessage(to, msg) {
+    return new Promise((err, res) => {
+        let options = {
+            url: process.env.META_URL,
+            method: `POST`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.META_ACCESS_TOKEN}`
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                "to": `${to}`,
+                "type": "template",
+                "template": {
+                    "name": "askbot",
+                    "language": {
+                        "code": "en"
+                    },
+                    "components": [
+                        {
+                            "type": "body",
+                            "parameters": [
+                                {
+                                    "type": "text",
+                                    "text": `${msg}`
+                                }
+                            ]
+                        }
+                    ]
+                }
+            })
+        };
+        (0, request_1.default)(options, (error, response, body) => {
+            if (error) {
+                return err(error);
+            }
+            if (response) {
+                return res(response);
+            }
+        });
+    });
+}
+app.get('/', (req, res) => {
+    return res.status(200).send("Welcome to METABOT");
 });
-app.get('/webhook', (request, response) => {
-    // var data = {};
-    // try{
-    //     data = {Request: request.body, Response: response};
-    // }catch(ex){
-    //     data = ex;
-    // }
-    return response.status(200).send("Hello Webhook");
+app.get('/sendmsg', (req, res) => {
+    var _a, _b;
+    var recepient = (_a = req === null || req === void 0 ? void 0 : req.query) === null || _a === void 0 ? void 0 : _a.to;
+    var message = (_b = req === null || req === void 0 ? void 0 : req.query) === null || _b === void 0 ? void 0 : _b.msg;
+    if (!recepient)
+        return res.status(500).send(`Recepient not provided!`);
+    if (!message)
+        return res.status(500).send(`Message not provided!`);
+    sendMessage(recepient, message).then(response => res.status(200).send(response)).catch(error => res.status(500).send(error));
 });
-app.post('/webhook', async (request, response) => {
+app.get('/webhook', (req, res) => {
     var data = {};
     try {
-        data = { Request: request.body, Response: response };
+        data = { Request: req.body, Response: res };
     }
     catch (ex) {
         data = ex;
     }
-    response.status(200).send(JSON.stringify(data));
+    res.status(200).send(JSON.stringify(data));
+});
+app.post('/webhook', async (req, res) => {
+    var data = {};
+    try {
+        data = { Request: req.body, Response: res };
+    }
+    catch (ex) {
+        data = ex;
+    }
+    res.status(200).send(JSON.stringify(data));
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,97 @@
 import express, { Request, Response } from 'express';
+import env from 'dotenv';
+import request from "request";
+
+env.config();
 
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (request: Request, response: Response) => {
-    return response.status(200).send("Welcome to METABOT");
+function sendMessage(to: string, msg: string){
+
+    return new Promise((err:any, res: any) => {
+        let options = {
+            url: process.env.META_URL,
+            method: `POST`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.META_ACCESS_TOKEN}`
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                "to": `${to}`,
+                "type": "template",
+                "template": {
+                    "name": "askbot",
+                    "language": {
+                        "code": "en"
+                    },
+                   "components": [
+                     {
+                       "type": "body",
+                       "parameters": [
+                           {
+                               "type": "text",
+                               "text": `${msg}`
+                           }
+                       ]
+                     }
+                   ]
+                }
+              })
+        };
+        
+        request(options, (error: any, response: any, body: any) => {        
+            if (error) {
+                return err(error);
+            }
+        
+            if (response) {
+                return res(response);
+            }
+        });
+
+    });
+}
+
+app.get('/', (req: Request, res: Response) => {
+    return res.status(200).send("Welcome to METABOT");
 });
 
-app.get('/webhook', (request: Request, response: Response) => {
+app.get('/sendmsg', (req: Request, res: Response) => {
+    var recepient: any = req?.query?.to;
+    var message: any = req?.query?.msg;
+
+    if(!recepient)
+        return res.status(500).send(`Recepient not provided!`);
+
+    if(!message)
+        return res.status(500).send(`Message not provided!`);
+
+    sendMessage(recepient, message).then(response => res.status(200).send(response)).catch(error => res.status(500).send(error));
+});
+
+app.get('/webhook', (req: Request, res: Response) => {
 
     // var data = {};
     // try{
-    //     data = {Request: request.body, Response: response};
+    //     data = {Request: req.body, Response: res};
     // }catch(ex){
     //     data = ex;
     // }
-    return response.status(200).send("Hello Webhook");
+    res.status(200).send(`Webhook[GET] hits`);
 });
 
-app.post('/webhook', async (request: Request, response: Response) => {
+app.post('/webhook', async (req: Request, res: Response) => {
 
-    var data = {};
-    try{
-        data = {Request: request.body, Response: response};
-    }catch(ex){
-        data = ex;
-    }
-    response.status(200).send(JSON.stringify(data));
+    // var data = {};
+    // try{
+    //     data = {Request: req.body, Response: res};
+    // }catch(ex){
+    //     data = ex;
+    // }
+    // res.status(200).send(JSON.stringify(data));
+    res.status(200).send(`Webhook[POST] hits`);
 });
 
 app.listen(port, () => {
